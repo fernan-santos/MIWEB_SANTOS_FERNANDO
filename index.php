d91b581b4086f0cde2b5814d7a8799966dd16cdf
Fernando Santos <75287002@continental.edu.pe>
2026-03-23 21:18:30 -0500

    cambio de nombre a index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Mostrar Nombre</title>
+</head>
+<body>
+    <?php
+
+        $nombre = "Juan Pérez";
+
+        echo "<h1>Hola, mi nombre es: " . $nombre . "</h1>";
+    ?>
+</body>
+</html>
