c6a2102b775a72f11797629a056f881be4addeb6
Fernando Santos <75287002@continental.edu.pe>
2026-03-23 21:15:09 -0500

    modificacion en eso.php

---
+++ eso.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Mostrar Nombre</title>
+</head>
+<body>
+    <?php
+        // 1. Colocar (definir) el nombre
+        $nombre = "Juan Pérez";
+        
+        // 2. Mostrar el nombre
+        echo "<h1>Hola, mi nombre es: " . $nombre . "</h1>";
+    ?>
+</body>
+</html>
